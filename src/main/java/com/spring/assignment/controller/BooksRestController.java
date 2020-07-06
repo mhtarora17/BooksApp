347af2e25dc292aec6d05c72163a7d6fbd79a7cb
mohitarora <mohitarora@ameyo.com>
2020-07-06 15:11:57 +0530

    Books project

---
+++ src/main/java/com/spring/assignment/controller/BooksRestController.java
@@ -0,0 +1,109 @@
+package com.spring.assignment.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.assignment.dto.BookDataObject;
+import com.spring.assignment.exception.ResourceNotFoundException;
+import com.spring.assignment.service.IBookService;
+
+/**
+ * The type Books controller.
+ *
+ * @author Mohit Arora
+ */
+@RestController
+@RequestMapping("/api/")
+public class BooksRestController {
+
+	@Autowired
+	@Qualifier(value = "bookService")
+	private IBookService iBookService;
+
+	/**
+	 * Get all Bookss list.
+	 *
+	 * @return the list
+	 */
+	@GetMapping("/books")
+	public List<BookDataObject> getAllBooks() {
+		return iBookService.getAllBooks();
+	}
+
+	/**
+	 * Gets Bookss by id.
+	 *
+	 * @param BooksId the Books id
+	 * @return the Bookss by id
+	 * @throws ResourceNotFoundException the resource not found exception
+	 */
+	@GetMapping("/book/{id}")
+	public ResponseEntity<BookDataObject> getBookById(@PathVariable(value = "id") Long bookId)
+			throws ResourceNotFoundException {
+		BookDataObject bookDataObject = iBookService.getBookById(bookId);
+		return ResponseEntity.ok().body(bookDataObject);
+	}
+
+	/**
+	 * Create Books Books.
+	 *
+	 * @param Books the Books
+	 * @return the Books
+	 */
+	@PostMapping("/book")
+	public BookDataObject createBook(@Valid @RequestBody BookDataObject bookDataObject) {
+		return iBookService.createBook(bookDataObject);
+	}
+
+	/**
+	 * Update Books response entity.
+	 *
+	 * @param bookId      the Books id
+	 * @param bookDetails the Books details
+	 * @return the response entity
+	 * @throws ResourceNotFoundException the resource not found exception
+	 */
+	@PutMapping("/book/{id}")
+	public ResponseEntity<BookDataObject> updateBooks(@PathVariable(value = "id") Long bookId,
+			@Valid @RequestBody BookDataObject bookDataObject) throws ResourceNotFoundException {
+		BookDataObject bookDataObject2 = iBookService.updateBooks(bookId, bookDataObject);
+		return ResponseEntity.ok(bookDataObject2);
+	}
+
+	/**
+	 * Delete Books map.
+	 *
+	 * @param bookId the Books id
+	 * @return the map
+	 * @throws Exception the exception
+	 */
+	@DeleteMapping("/book/{id}")
+	public Map<String, Boolean> deleteBooks(@PathVariable(value = "id") Long bookId) throws Exception {
+		Boolean deleted = false;
+		try {
+			deleted = iBookService.deleteBooks(bookId);
+		}catch (Exception e) {
+			deleted = false;
+		}
+		
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", deleted);
+		return response;
+	}
+}
---
+++ src/main/java/com/spring/assignment/model/Book.java
@@ -0,0 +1,109 @@
+package com.spring.assignment.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "books")
+@EntityListeners(AuditingEntityListener.class)
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "book_name", nullable = false)
+	private String bookName;
+	
+	@Column(name = "author_name", nullable = false)
+	private String authorName;
+
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at", nullable = false)
+	private Date createdAt;
+
+	@Column(name = "created_by", nullable = false)
+	@CreatedBy
+	private String createdBy;
+
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at", nullable = false)
+	private Date updatedAt;
+
+	@Column(name = "updated_by", nullable = false)
+	@LastModifiedBy
+	private String updatedBy;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+}
